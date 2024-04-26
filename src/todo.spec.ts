24f7d3d379a5d95a548f62e3dd29b742fa0f4d69
DS <ds@DSs-MacBook-Pro.lan>
2024-04-26 17:13:38 +0200

    feat/convert-input - add functions to convert input.json to output.json

--- src/todo.spec.ts
+++ src/todo.spec.ts
@@ -1,19 +1,56 @@
 import { expect } from 'chai';
 import inputJson from './input.json';
 import outputJson from './output.json';
-import { convertInput } from './todo';
-import { Input } from './types/input';
+import { convertInput, sortAnnotations, sortEntities } from './todo';
 
-describe('Todo', () => {
-  // TODO: make sure this test passes
-  it('Should be able to convert the input (flat lists) to the output (nested) structure', () => {
+import { Input, EntityClass, EntityType } from './types/input';
+
+describe.only('convertInput function', () => {
+  it('should have the right entities length after conversion', () => {
     const output = convertInput(inputJson as Input);
-
-    expect(output.documents.length).to.equal(1);
     expect(output.documents[0].entities.length).to.equal(14);
-    expect(output.documents[0].annotations.length).to.equal(9);
-    expect(output).to.deep.equal(outputJson);
   });
 
+  it('should have the right annotations length after conversion', () => {
+    const output = convertInput(inputJson as Input);
+    expect(output.documents[0].annotations.length).to.equal(9);
+  });
+
+  // TODO: make sure this test passes
+  it('should be able to convert the input (flat lists) to the output (nested) structure', () => {
+    const output = convertInput(inputJson as Input);
+    expect(output.documents.length).to.equal(1);
+
+    expect(output).to.deep.equal(outputJson);
+  });
+});
+
+describe('sorting functions', () => {
+  const mockEntities = [
+    { id: '1', name: 'article total', type: EntityType.NUMBER, class: EntityClass.TEXT, children: [] },
+    { id: '2', name: 'article color', type: EntityType.REGULAR, class: EntityClass.TEXT, children: [] },
+    { id: '3', name: 'article', type: EntityType.REGULAR, class: EntityClass.RELATION, children: [] },
+  ];
+
+  const mockAnnotations = [
+    { id: 'a1', entity: { id: '1', name: 'article total' }, value: 394.68, index: 88, children: [] },
+    { id: 'a2', entity: { id: '2', name: 'article color' }, value: 'White', index: 96, children: [] },
+    { id: 'a3', entity: { id: '3', name: 'article' }, value: null, index: 72, children: [] },
+  ];
+
+  it('should sortEntities by "name"', () => {
+    mockEntities.sort(sortEntities);
+    expect(mockEntities.map((e) => e.name)).to.deep.equal(['article', 'article color', 'article total']);
+  });
+
+  it('should sortAnnotations by index', () => {
+    mockAnnotations.sort(sortAnnotations);
+    expect(mockAnnotations.map((a) => a.index)).to.deep.equal([72, 88, 96]);
+  });
+});
+
+describe('convertEntity function', () => {});
+describe('convertAnnotation function', () => {});
+describe('validateOutput function', () => {
   // BONUS: Write tests that validates the output json. Use the function you have written in "src/todo.ts".
 });
--- src/todo.ts
+++ src/todo.ts
@@ -1,39 +1,111 @@
 import { Annotation, Entity, Input } from './types/input';
 import { ConvertedAnnotation, ConvertedEntity, Output } from './types/output';
 
-// TODO: Convert Input to the Output structure. Do this in an efficient and generic way.
-// HINT: Make use of the helper library "lodash"
+interface EntityMap {
+  [key: string]: ConvertedEntity;
+}
+interface AnnotationMap {
+  [key: string]: ConvertedAnnotation;
+}
+
 export const convertInput = (input: Input): Output => {
   const documents = input.documents.map((document) => {
-    // TODO: map the entities to the new structure and sort them based on the property "name"
-    // Make sure the nested children are also mapped and sorted
-    const entities = document.entities.map(convertEntity).sort(sortEntities);
+    const entityMap: EntityMap = {},
+      annotationMap: AnnotationMap = {},
+      annotations: ConvertedAnnotation[] = [];
 
-    // TODO: map the annotations to the new structure and sort them based on the property "index"
-    // Make sure the nested children are also mapped and sorted
-    const annotations = document.annotations.map(convertAnnotation).sort(sortAnnotations);
-    return { id: document.id, entities, annotations };
+    document.entities.forEach((entity) => {
+      entityMap[entity.id] = {
+        ...{ id: entity.id, name: entity.name, type: entity.type, class: entity.class },
+        children: [],
+      };
+    });
+
+    document.annotations.forEach((annotation) => {
+      annotationMap[annotation.id] = {
+        ...{
+          id: annotation.id,
+          entity: {
+            id: entityMap[annotation.entityId].id,
+            name: entityMap[annotation.entityId].name,
+          },
+          value: annotation.value,
+          index: -1,
+          children: [],
+        },
+      };
+    });
+
+    const entities: ConvertedEntity[] = document.entities
+      .map((entity) => convertEntity(entity, entityMap))
+      .sort(sortEntities);
+
+    document.annotations.forEach((annotation) => {
+      annotation.refs.length < 1
+        ? annotations.push(convertAnnotation(annotation, annotationMap))
+        : convertAnnotation(annotation, annotationMap);
+    });
+
+    annotations.sort(sortAnnotations);
+
+    return {
+      id: document.id,
+      entities,
+      annotations,
+    };
   });
 
   return { documents };
 };
 
-// HINT: you probably need to pass extra argument(s) to this function to make it performant.
-const convertEntity = (entity: Entity): ConvertedEntity => {
-  throw new Error('Not implemented');
+const convertEntity = (entity: Entity, entityMap: EntityMap): ConvertedEntity => {
+  entity.refs.forEach((refId: string) => {
+    if (entityMap[refId]) {
+      entityMap[refId].children.push({ ...entityMap[entity.id] });
+    }
+  });
+
+  entityMap[entity.id].children.sort(sortEntities);
+
+  return entityMap[entity.id];
 };
 
-// HINT: you probably need to pass extra argument(s) to this function to make it performant.
-const convertAnnotation = (annotation: Annotation): ConvertedAnnotation => {
-  throw new Error('Not implemented');
+const convertAnnotation = (annotation: Annotation, annotationMap: AnnotationMap): ConvertedAnnotation => {
+  try {
+    if (!annotationMap[annotation.id]) {
+      throw new Error(`Annotation with ID ${annotation.id} not found.`);
+    }
+
+    if (annotation.indices && annotation.indices.length > 0) {
+      annotationMap[annotation.id].index = annotation.indices[0].start;
+    } else if (annotationMap[annotation.id].children.length > 0) {
+      annotationMap[annotation.id].children.sort(sortAnnotations);
+      annotationMap[annotation.id].index = annotationMap[annotation.id].children[0].index;
+    } else {
+      throw new Error('Cannot assign index for annotation.');
+    }
+
+    annotation.refs.forEach((refId: string) => {
+      if (!annotationMap[refId]) {
+        throw new Error(`Reference annotation with ID ${refId} not found.`);
+      }
+
+      annotationMap[refId].children.push(annotationMap[annotation.id]);
+    });
+
+    return annotationMap[annotation.id];
+  } catch (error) {
+    error instanceof Error && console.error(`Error converting annotation: ${error.message}`);
+    throw error;
+  }
 };
 
-const sortEntities = (entityA: ConvertedEntity, entityB: ConvertedEntity) => {
-  throw new Error('Not implemented');
+export const sortEntities = (entityA: ConvertedEntity, entityB: ConvertedEntity): number => {
+  return entityA.name.localeCompare(entityB.name);
 };
 
-const sortAnnotations = (annotationA: ConvertedAnnotation, annotationB: ConvertedAnnotation) => {
-  throw new Error('Not implemented');
+export const sortAnnotations = (annotationA: ConvertedAnnotation, annotationB: ConvertedAnnotation): number => {
+  return annotationA.index - annotationB.index;
 };
 
 // BONUS: Create validation function that validates the result of "convertInput". Use yup as library to validate your result.
